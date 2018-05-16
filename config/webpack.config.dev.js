813c38205d1fa2b1e28a57bd2b26460b36ce630c
Wfy9526 <819187726@qq.com>
2018-05-16 22:24:35 +0800

    基本home页

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -139,10 +139,10 @@ module.exports = {
           // smaller than specified limit in bytes as data URLs to avoid requests.
           // A missing `test` is equivalent to a match.
           {
-            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
             loader: require.resolve('url-loader'),
             options: {
-              limit: 10000,
+              limit: 40960,
               name: 'static/media/[name].[hash:8].[ext]',
             },
           },
--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,18 +1,84 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-import './home.less';
-import { Button , Steps  } from 'antd';
+import { connect } from "react-redux";
+import "./home.less";
+import { Menu, Icon, Layout } from "antd";
+import svg from "@/image/logo.svg"
+
+const { Header, Footer, Sider, Content } = Layout;
+const { SubMenu } = Menu;
 
 class Home extends Component {
-    render() {
-        return (
-            <div className="parent-container">
-                <div className="text">
-                    hello,world!!
-                </div>
-            </div>
-        );
-    }
+  handleClick = e => {
+    console.log("click ", e);
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <Layout className="wrap-container">
+          <Header className="header">
+            <img src={svg} className="rotate" />
+            <span>练手项目</span>
+          </Header>
+          <Layout>
+            <Sider>
+              <Menu
+                mode={"inline"}
+                inlineCollapsed={false}
+                style={{ width: 256 }}
+                defaultSelectedKeys={["1"]}
+                defaultOpenKeys={["sub1", "sub2", "sub3"]}
+              >
+                <Menu.Item key="1">
+                  <Icon type="mail" />
+                  个人中心
+                </Menu.Item>
+
+                <SubMenu
+                  key="sub1"
+                  title={
+                    <span>
+                      <Icon type="calendar" />
+                      <span>我的订单</span>
+                    </span>
+                  }
+                >
+                  <Menu.Item key="2">近三个月订单</Menu.Item>
+                </SubMenu>
+
+                <SubMenu
+                  key="sub2"
+                  title={
+                    <span>
+                      <Icon type="appstore" />
+                      <span>我的资产</span>
+                    </span>
+                  }
+                >
+                  <Menu.Item key="3">我的红包</Menu.Item>
+                  <Menu.Item key="4">账户余额</Menu.Item>
+                  <Menu.Item key="5">我的金币</Menu.Item>
+                </SubMenu>
+                <SubMenu
+                  key="sub3"
+                  title={
+                    <span>
+                      <Icon type="setting" />
+                      <span>我的资料</span>
+                    </span>
+                  }
+                >
+                  <Menu.Item key="7">个人资料</Menu.Item>
+                  <Menu.Item key="8">地址管理</Menu.Item>
+                </SubMenu>
+                <Menu.Item key="9">我的收藏</Menu.Item>
+              </Menu>
+            </Sider>
+            <Content>main content</Content>
+          </Layout>
+        </Layout>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Home;
