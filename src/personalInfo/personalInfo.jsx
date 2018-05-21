faf96dc93dff56c8fcfaee29bf342e84e4ee27b0
Wfy9526 <819187726@qq.com>
2018-05-22 00:02:44 +0800

    个人中心-个人信息

--- src/personalInfo/personalInfo.jsx
+++ src/personalInfo/personalInfo.jsx
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'antd';
+import './personalInfo.less';
+import echarts from 'echarts'
 
-export default class PersonalInfo extends Component{
-    render(){
-        debugger
-        return (<div>/</div>);
+export default class PersonalInfo extends Component {
+    render() {
+
+        return (<React.Fragment>
+            <Row gutter={24} className="personal-info">
+                <Col span={9}>
+                    <Col span={8}>
+                        <div className="head-photo"></div>
+                    </Col>
+                    <Col span={16} className="sub-title">
+                        <div>夜已深, {}</div>
+                        <span>是不是饿的睡不着呀？吃个夜宵呗！</span>
+                    </Col>
+                </Col>
+                <Col span={5}>
+                    <div>我的红包</div>
+                    <div><span>{5}</span>个</div>
+                </Col>
+                <Col span={5}>
+                    <div>我的金币</div>
+                    <div><span>{5}</span>个</div>
+                </Col>
+                <Col span={5}>
+                    <div>账户余额</div>
+                    <div><span>{5}</span>个</div>
+                </Col>
+            </Row>
+        </React.Fragment>);
     }
 }
